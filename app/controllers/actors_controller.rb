fa6ee8ebf32f0e57bda14e8fabf96aa7b2a08e18
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-09-27 16:46:54 +0000

    Actor Functionality

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,37 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    the_actor = Actor.new
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.name = params.fetch("query_name")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_actor = Actor.where({ :id => the_id }).at(0)
+
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.name = params.fetch("query_name")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_actor = Actor.find(the_id)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+  end
 end
